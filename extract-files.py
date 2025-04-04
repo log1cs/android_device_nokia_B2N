f9d382298fc272bde14e3151309f815c25522aff
Nolen Johnson <johnsonnolen@gmail.com>
2025-04-04 09:55:01 +0700

    B2N: Migrate to Python extract-utils
    Change-Id: I677baabde97763a8adeb6f0b5e2f64bde3ae6677

---
+++ extract-files.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/nokia/sdm660-common',
+    'hardware/qcom-caf/msm8998',
+    'vendor/nokia/sdm660-common',
+]
+
+module = ExtractUtilsModule(
+    'B2N',
+    'nokia',
+    namespace_imports=namespace_imports,
+    add_firmware_proprietary_file=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'sdm660-common', module.vendor
+    )
+    utils.run()
